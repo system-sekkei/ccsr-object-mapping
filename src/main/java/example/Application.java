5ba820d2eadefca59b4ecd1eb33468e84c92c9f1
irof <irof.ocean@gmail.com>
2020-05-11 14:55:28 +0900

    ObjectMapperの生成をSpringBootに任せる

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,21 +1,13 @@
 package example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import example.infrastructure._configuration.CustomObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ObjectMapper customObjectMapper() {
-        return CustomObjectMapper.create();
     }
 
 }
